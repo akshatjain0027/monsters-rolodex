c1f834bbf9da3639d3beb01f47f03217faa691fd
Akshat Jain <akshatjain0027@gmail.com>
2019-11-12 19:39:24 +0530

    Updates

---
+++ precache-manifest.bdd14e39a42d7e95c1f1b88be105a38b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1e20501e794e3d329ad6b573ff75809",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "f63704ffb6e5ac12e976",
+    "url": "/monsters-rolodex/static/css/main.255ef1cb.chunk.css"
+  },
+  {
+    "revision": "8f66e7892f88ac220728",
+    "url": "/monsters-rolodex/static/js/2.e8d38444.chunk.js"
+  },
+  {
+    "revision": "f63704ffb6e5ac12e976",
+    "url": "/monsters-rolodex/static/js/main.f2556a18.chunk.js"
+  },
+  {
+    "revision": "7f410422c000bddbff9e",
+    "url": "/monsters-rolodex/static/js/runtime-main.a7e40423.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/monsters-rolodex/static/media/logo.25bf045c.svg"
+  }
+]);
